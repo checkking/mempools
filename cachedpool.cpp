7e68617da12865031ae03b4f3a93c8a81e8690b4
root <checkking@foxmail.com>
2017-04-18 00:59:38 +0800

    x

--- cachedpool.cpp
+++ cachedpool.cpp
@@ -3,15 +3,15 @@
 namespace checkking {
 namespace mempool {
 void* cachedpool::malloc(size_t size) {
-	size = (size + 7 ) & ( -1 << 3); // alignment, -1(0xffff)
+    size = (size + 7 ) & ( -1 << 3); // alignment, -1(0xffff)
     if (size < _s_seg_size - sizeof(void *)) {
         if (size > _buf_size) {
             void* tmp = ::malloc(_s_seg_size);
             if (tmp) {
-				*(void **)tmp = _memlist;
-				_memlist = tmp;
-				_free_space = (char *)tmp + sizeof(void *);
-				_buf_size = _s_seg_size - sizeof(void *);
+                *(void **)tmp = _memlist;
+                _memlist = tmp;
+                _free_space = (char *)tmp + sizeof(void *);
+                _buf_size = _s_seg_size - sizeof(void *);
             } else {
                 return 0;
             }
